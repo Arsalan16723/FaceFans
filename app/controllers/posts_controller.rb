231a642392b86347910aae4d63a4d4c3695d97cc
Arsalan <m.arsalan16723@gmail.com>
2016-10-01 08:02:22 +0000

    CRUD operation on Posts completed STAGE#1 completed

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+	def index
+		@posts = Post.all
+	end
+
+
+	def new
+		@post = Post.new
+	end
+
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			flash[:success] = "Post created Successfully"
+			redirect_to posts_path
+		else
+			flash.now[:alert] = "Couldn't create post at this time"
+			render 'new'
+		end	
+	end
+
+
+	def show
+		
+	end
+
+
+	def edit
+		
+
+	end
+
+	def update
+		if @post.update(post_params)
+			flash[:success] = "Post updated"
+			redirect_to(post_path(@post))
+		else
+			flash.now[:alert] = "Could not update check your data"
+			render 'edit'
+		end
+	end
+
+	def destroy
+		
+		@post.destroy
+		redirect_to posts_path
+	end
+
+
+	private
+
+	def post_params
+		params.require(:post).permit(:image, :caption)
+	end
+	def set_post
+		@post = Post.find(params[:id])
+	end
+end
